e918dc0e9da3e72c3e8d09686d055219e87d1107
Abhinav Sharma <abhinav.161a026@gmail.com>
2021-10-20 20:44:37 +0530

    second commit

--- login.js
+++ login.js
@@ -2,20 +2,35 @@ window.addEventListener('load',function(){
     const btn= document.getElementById('login')
     btn.addEventListener('click',handleLogin)
 })
-const handleLogin= ()=>{
-    const email= document.getElementById('email').value
-    document.getElementById('email').value=''
-    const password= document.getElementById('password').value
-    document.getElementById('password').value=''
-    const payLoad= {
-        email,
-        password
+const handleLogin= async ()=>{
+    try{
+        const email= document.getElementById('email').value
+        document.getElementById('email').value=''
+        const password= document.getElementById('password').value
+        document.getElementById('password').value=''
+        const payLoad= {
+            email,
+            password
+        }
+        const response= await loginUser(payLoad)
+        if(!response.token){
+            alert(`${response.error}. Please try again`)
+        }
+        else{
+            window.location.replace("home.html")
+        }
     }
-    console.log(payLoad)
+    catch(err){
+        console.log(err)
+    }
 }
 
-const loginUser=(email,password)=>{
+const loginUser=(payload)=>{
     return fetch('https://reqres.in/api/login',{
-
-    })
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)
+    }).then(res=>res.json())
 }
